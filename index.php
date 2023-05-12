74ed98cc16ef7da8e3ef23f2771415179485f1f4
GinoNielo <133355814+GinoNielo@users.noreply.github.com>
2023-05-12 12:33:04 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+   <?php
+  
+    $getal1 = 12;
+    $getal = 10;
+    $Result = $getal1 + $getal;
+    echo $Result;
+
+    $getal1 = 12;
+    $getal = 10;
+
+    $Result = $getal1 * $getal ;
+    echo $Result;
+
+    $naam = "Gino";
+
+    echo "welkom ".$naam; 
+    ?>
+    
+</body>
+</html>
