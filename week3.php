b7e3b1c2d4457f31837af51d13591b94c596a6fe
GinoNielo <133355814+GinoNielo@users.noreply.github.com>
2023-05-25 15:49:56 +0200

    Add files via upload

---
+++ week3.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>array</title>
+</head>
+<body>
+  
+<?php
+ $myArray = ['auto','fiets','brommer','bus','vliegtuig','trein']; //deel 1
+
+ echo $myArray [0];
+ echo print_r($myArray);  
+ echo "<br>";
+
+$myArray = ['auto','fiets','brommer','bus','vliegtuig','trein']; //deel 2
+
+$myArray[] = 'scooter';
+
+
+
+echo print_r($myArray);
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
+</body>
